f497ec6f323bc1a93f0f66c5ba2fcea8375bdf72
IsaevAlexandr <aisaev188@gmail.com>
2017-10-28 18:21:12 +0300

    28.10.17 edited gnome sort

--- GnomeSort.js
+++ GnomeSort.js
@@ -11,59 +11,33 @@ function swap(array, firstIndex, secondIndex) {
 }
 
 /**
- * Gnome sort algorithm. Changing incoming array           
- * @param {*Array} array
- * @param {*Boolean} log results
- * @return {Array} 
+ * Gnome sort algorithm           
+ * @param {Array} array
+ * @return {Array} sortedArray
  */
-function gnomeSort(array, log = true) {
-    let i = 1, 
-    	temp,
-    	len = array.length,
-    	compareCount = 1;
+function gnomeSort(array) {
+    let sortedArray = array.slice();
+    let i = 1,
+        len = sortedArray.length;
 
-    while(i < len) {
-        /*If the pointer is longer than the length of the array, we interrupt the loop*/
-        compareCount++;
-
-        /*if the previous element is smaller than the next one, move the pointer to the right,
-        until pointer longer than the length of the array*/
-        if( i == 0 || array[i - 1] <= array[i] ){
+    while (i < len) {
+        if (i == 0 || sortedArray[i - 1] <= sortedArray[i]) {
             i++;
-        }
-
-        /*otherwise we change the elements by places and move the pointer to the left*/
-        else {
-       		swap(array, i, i -1)
+        } else {
+            swap(sortedArray, i, i - 1)
             i--;
         }
     }
-
-
-    /* if some one know better way to show @compareCount parametr, pls let me know!*/
-    function showInfo(){
-        console.log(`/*======================================*/`);
-        console.log(`${this.name} algorithm`);
-        console.log(`Array length : ${this.arguments[0].length}`);
-        console.log(`Number of comparisons : ${compareCount}`);
-        console.log(`resuls : ${array}`);
-        console.log(`/*======================================*/`);
-    }
-    
-    if(log){
-        showInfo.call(gnomeSort);
-    }
-
-    return array;
+    return sortedArray;
 }
 
 /*tests*/
-let arr = [1,2,3,4,5,6,7,8,9,10]
-let arr2 = [3,7,2,1];
-let arr3 = [1,2,3,4,5,6,7,8,10,9];
-let arr4 = [-10,2,-4,4,0,155,6,-7,8,10,9];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let arr2 = [3, 7, 2, 1];
+let arr3 = [2, 1, 3, 4, 5, 6, 7, 8, 10, 9];
+let arr4 = [-10, 2, -4, 4, 0, 155, 6, -7, 8, 10, 9];
 
-gnomeSort(arr);
-gnomeSort(arr2);
-gnomeSort(arr3);
-gnomeSort(arr4);
+console.log(gnomeSort(arr));
+console.log(gnomeSort(arr2));
+console.log(gnomeSort(arr3));
+console.log(gnomeSort(arr4));
